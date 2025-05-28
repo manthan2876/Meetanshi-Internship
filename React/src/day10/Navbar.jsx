e08d4daa3717c9364ff96cfdbe2f542d04124da3
Manthan Kuvadiya <23it050@charusat.edu.in>
2025-05-28 14:43:55 +0530

    day10 taks react static page

---
+++ React/src/day10/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img className="react-logo" src="/react.svg" alt="React Logo" />
+        <span className="react-facts-title">ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
