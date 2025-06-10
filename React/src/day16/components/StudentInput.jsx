3b317145f6e70d9ec2ff5f167515e9ae758b80c4
Manthan Kuvadiya <23it050@charusat.edu.in>
2025-06-10 09:45:37 +0530

    error message dialogbox added

--- React/src/day16/components/StudentInput.jsx
+++ React/src/day16/components/StudentInput.jsx
@@ -124,7 +124,7 @@ export function StudentInput({
                 </div>
                 <div className="flex items-center justify-between">
                     <button
-                        className={`bg-${editMode ? "green" : "blue"}-500 hover:bg-${editMode ? "green" : "blue"}-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+                        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
                         type="submit"
                     >
                         {editMode ? "Update Student" : "Add Student"}
